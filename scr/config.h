7fa5bdeb541d2fc61f5a8c4913b6d490ead22f9b
bitsamrat <bitsamraat@gmail.com>
2016-11-08 00:45:06 +0530

    Update config.h

--- scr/config.h
+++ scr/config.h
@@ -1,1 +1,4 @@
 const char CRYPTONOTE_NAME[] = "nemisiscoin";
+const uint64_t MONEY_SUPPLY = (uint64_t)(-1); 
+ const unsigned EMISSION_SPEED_FACTOR = 18; 
+ const uint64_t DIFFICULTY_TARGET = 120; 
